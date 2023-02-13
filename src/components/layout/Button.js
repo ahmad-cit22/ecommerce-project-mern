cf3f0c3f7a2737698055bf9e89573e38e148fcd7
Nafis Abdullah <n.ahmad.web.cit22@gmail.com>
2023-02-13 08:39:42 +0600

    updated

--- src/components/layout/Button.js
+++ src/components/layout/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ title }) => {
   return (
-    <button className="bg-primary py-4 px-24 font-dm text-sm font-bold text-white">
+    <button className="bg-primary py-4 px-24 font-dm text-sm font-bold text-[#fff]">
       {title}
     </button>
   );
